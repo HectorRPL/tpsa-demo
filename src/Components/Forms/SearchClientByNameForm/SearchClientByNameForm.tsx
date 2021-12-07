7546b071c0b69c1fcfa1ec669d21dd8f400e7b94
HFV <hector.kaizen@gmail.com>
2021-12-07 11:26:03 -0600

    QF styles

--- src/Components/Forms/SearchClientByNameForm/SearchClientByNameForm.tsx
+++ src/Components/Forms/SearchClientByNameForm/SearchClientByNameForm.tsx
@@ -1,6 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import React from 'react';
+import Box from '@mui/material/Box';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,15 +17,20 @@ const SearchClientByNameForm = () => {
   const classes = useStyles();
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="name" label="Nombre"/>
-      <br/>
-      <TextField id="firstName" label="Apellido paterno"/>
-      <br/>
-      <TextField id="lastName" label="Apellido materno"/>
-      <br/>
-      <TextField id="birthDate" label="Fecha de nacimiento"/>
-    </form>
+    <Box
+      sx={{
+        width: 1000,
+        maxWidth: '100%',
+      }}
+    >
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="name" label="Nombre" fullWidth/>
+        <TextField id="firstName" label="Apellido paterno"/>
+        <TextField id="lastName" label="Apellido materno"/>
+        <TextField id="birthDate" label="Fecha de nacimiento"/>
+      </form>
+    </Box>
+
   );
 }
 

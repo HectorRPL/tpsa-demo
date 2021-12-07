bb4871f70c660b7396e8acd4371ba81f79ae7546
HFV <hector.kaizen@gmail.com>
2021-12-07 11:25:34 -0600

    add Redirect

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Layout from './Views/Layout/Layout';
 import Auth from './Views/Auth/Auth';
@@ -7,17 +7,12 @@ const Routing = () => {
   return (
     <Switch>
 
-      {
-        /*
-        Problems with redirect implementation
-        <Route path='' exact>
-         <Redirect to='/signIn'/>
-       </Route>*/
-      }
+      <Route path="/" exact>
+        <Redirect to="/auth/signIn"/>
+      </Route>
 
       <Route path="/auth" component={Auth}/>
       <Route path="/layout" component={Layout}/>
-
 
     </Switch>
   )
